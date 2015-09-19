611f108128322118beed225b2233c1ae9fcd902e
Paul <linp21847@gmail.com>
2015-09-19 10:42:11 +0200

    GetAnchorText end point added

--- Majestic.php
+++ Majestic.php
@@ -5,20 +5,58 @@ class Majestic
 {
 	private $cmd = null;
 	private $itemsCount = 1;
+	private $count = 100;
 	private $itemsList = array();
 	private $dataSource = 'fresh';
 
-	private function buildUrl() {
+	public function GetIndexItemInfo($cmd=NULL, $items=array(), $dataSource="fresh") {
 		global $api_key;
-		$cmd = $this->cmd;
-		$items = $this->items;
-		$itemsCount = $this->itemsCount;
-		$dataSource = $this->dataSource;
+		$itemsCount = sizeof($items);
 
 		$baseUrl = "http://api.majestic.com/api/json?app_api_key=$api_key&cmd=$cmd&items=$itemsCount&datasource=$dataSource";
 
 		foreach ($items as $key => $value) {
 			$baseUrl .= '&item' . $key . "=$value";
+		}
+
+		return $this->getResult($baseUrl);
+	}
+
+	public function GetBackLinkData($cmd=NULl, $item=NULL, $count=1, $dataSource="fresh") {
+		global $api_key;
+
+		$baseUrl = "http://api.majestic.com/api/json?app_api_key=$api_key&cmd=$cmd&item=$item&Count=$count&datasource=$dataSource";
+		return $this->getResult($baseUrl);
+	}
+
+	public function GetAnchorText($cmd=NULl, $item=NULL, $count=1, $dataSource="fresh") {
+		global $api_key;
+
+		$baseUrl = "http://api.majestic.com/api/json?app_api_key=$api_key&cmd=$cmd&item=$item&Count=$count&datasource=$dataSource";
+		return $this->getResult($baseUrl);
+	}
+
+	private function buildUrl() {
+		global $api_key;
+		$cmd = $this->cmd;
+		$baseUrl = "";
+
+		switch ($cmd) {
+			case 'GetBackLinkData':
+				$items = $this->items;
+				$itemsCount = $this->itemsCount;
+				$dataSource = $this->dataSource;
+
+				$baseUrl = "http://api.majestic.com/api/json?app_api_key=$api_key&cmd=$cmd&items=$itemsCount&datasource=$dataSource";
+
+				foreach ($items as $key => $value) {
+					$baseUrl .= '&item' . $key . "=$value";
+				}
+				break;
+			
+			default:
+				# code...
+				break;
 		}
 		return $baseUrl;
 	}
@@ -27,7 +65,7 @@ class Majestic
 		$ch = curl_init();
 
 		// set url 
-		curl_setopt($ch, CURLOPT_URL, $this->buildUrl()); 
+		curl_setopt($ch, CURLOPT_URL, $url); 
 
 		//return the transfer as a string 
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
@@ -39,13 +77,6 @@ class Majestic
 		curl_close($ch);
 
 		return json_decode($output);
-	}
-
-	public function setParams($cmd=0, $items=array(), $dataSource="fresh") {
-		$this->cmd = $cmd;
-		$this->items = $items;
-		$this->itemsCount = sizeof($items);
-		$this->dataSource = $dataSource;
 	}
 }
 
--- api.php
+++ api.php
@@ -3,15 +3,38 @@
 
 	header('Access-Control-Allow-Origin: *');
 	$mine = new Majestic();
+	$result;
 	
 
 	if (isset($_GET) && !empty($_GET)) {
 		$cmd = $_GET['cmd'];
-		$items = $_GET['items'];
 		$dataSource = $_GET['dataSource'];
 
-		$mine->setParams($cmd, $items, $dataSource);
+		switch ($cmd) {
+			case 'GetIndexItemInfo':
+				$items = $_GET['items'];
+				$result = $mine->GetIndexItemInfo($cmd, $items, $dataSource);
+				break;
+
+			case 'GetBackLinkData':
+				$item = $_GET['item'];
+				$count = $_GET['count'];
+				$result = $mine->GetBackLinkData($cmd, $item, $count, $dataSource);
+				break;
+
+			case 'GetAnchorText':
+				$item = $_GET['item'];
+				$count = $_GET['count'];
+				$result = $mine->GetAnchorText($cmd, $item, $count, $dataSource);
+				break;
+			
+			default:
+				# code...
+				break;
+		}
+
+		
 	}
 
-	echo json_encode($mine->getResult());
+	echo json_encode($result);
 ?>
